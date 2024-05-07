92ecbe6b8d8364807cb6fa1b8f1b96262903b354
Brandon Hoffman <brandon.hoffman002@student.swic.edu>
2024-05-07 14:50:13 -0500

    Select the submit and reset button

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ const formInputs = [
   document.querySelector("#textbox"),
 ];
 console.log(formInputs);
+
+const formButtons = [...Array.from(document.querySelectorAll("button"))];
+console.log(formButtons);
